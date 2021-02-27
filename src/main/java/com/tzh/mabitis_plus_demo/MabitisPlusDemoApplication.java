5b5179448a99f5e8b63ae78e4085ea6328811c50
TZH <13164377125@163.com>
2021-02-27 13:35:33 +0800

    创建mybatis_plus_demo

---
+++ src/main/java/com/tzh/mabitis_plus_demo/MabitisPlusDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tzh.mabitis_plus_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabitisPlusDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MabitisPlusDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tzh/mabitis_plus_demo/MabitisPlusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzh.mabitis_plus_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabitisPlusDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
